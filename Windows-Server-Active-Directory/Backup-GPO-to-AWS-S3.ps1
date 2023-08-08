c1b803d637ea3844d1688d1cc24c0f4bac9b5fff
Ryan Viglione <86791607+ViggoMode2021@users.noreply.github.com>
2023-08-08 07:49:29 -0400

    Update Backup-GPO-to-AWS-S3.ps1

--- Windows-Server-Active-Directory/Backup-GPO-to-AWS-S3.ps1
+++ Windows-Server-Active-Directory/Backup-GPO-to-AWS-S3.ps1
@@ -1,8 +1,18 @@
-#https://docs.aws.amazon.com/powershell/latest/userguide/pstools-s3-bucket-create.html
+#https://docs.aws.amazon.com/powershell/latest/userguide/pstools-s3-bucket-create.html - Setup
+
+#https://docs.aws.amazon.com/powershell/latest/reference/items/S3_cmdlets.html - Commands
 
 Import-Module AWS.Tools.Common
 
-#Set-AWSCredential -AccessKey "" -SecretKey "" # Put AWS keys here if needed
+Write-Host "Welcome to Windows Server backup to AWS S3. With this script, you can backup data from your Windows Server environment to
+AWS S3." -ForegroundColor "Green"
+
+$Access_Key = Read-Host "Please put your AWS Access Key here. *Note that your generated AWS credentials should only contain the 'AmazonS3FullAccess'
+policy."
+
+$Secret_Access_Key = Read-Host "Please put your AWS Secret Access key here."
+
+Set-AWSCredential -AccessKey $Access_Key -SecretKey $Secret_Access_Key
 
 $Bucket_Name = Read-Host "What would you like to name your s3 bucket?"
 
@@ -12,6 +22,8 @@ $Desktop = [Environment]::GetFolderPath("Desktop")
 
 New-Item -Path $Desktop -Name "Group-Policy-backups" -ItemType "Directory"
 
+Write-S3Object -BucketName $Bucket_Name -KeyPrefix "\" -Folder "$Desktop\Group-Policy-backups"
+
 Backup-GPO -All -Path C:\Users\Administrator\desktop\Group-Policy-backups
 
-Write-S3Object -BucketName $Bucket_Name -KeyPrefix "\" -Folder "C:\Users\Administrator\desktop\Group-Policy-backups" -Recurse
+Write-S3Object -BucketName $Bucket_Name -KeyPrefix "\Group-Policy-backups" -Folder "C:\Users\Administrator\desktop\Group-Policy-backups" -Recurse
