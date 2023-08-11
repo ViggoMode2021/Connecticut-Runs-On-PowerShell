88de3425b69b919437acbb6c8e5af40d30db788e
Ryan Viglione <86791607+ViggoMode2021@users.noreply.github.com>
2023-08-10 21:03:15 -0400

    Update Backup-GPO-to-AWS-S3.ps1

--- Windows-Server-Active-Directory/Backup-GPO-to-AWS-S3.ps1
+++ Windows-Server-Active-Directory/Backup-GPO-to-AWS-S3.ps1
@@ -14,16 +14,46 @@ $Secret_Access_Key = Read-Host "Please put your AWS Secret Access key here."
 
 Set-AWSCredential -AccessKey $Access_Key -SecretKey $Secret_Access_Key
 
+do{
+
 $Bucket_Name = Read-Host "What would you like to name your s3 bucket?"
+
+if(Test-S3Bucket -BucketName $Bucket_Name){
+
+Write-Host "Bucket named $Bucket_Name already exists." -ForegroundColor Red
+
+}
+
+}
+
+until(!(Test-S3Bucket -BucketName $Bucket_Name))
+
+Write-Host "Creating bucket named $Bucket_Name." -ForegroundColor Green
 
 New-S3Bucket -BucketName $Bucket_Name -Region us-east-1
 
 $Desktop = [Environment]::GetFolderPath("Desktop")
 
-New-Item -Path $Desktop -Name "Group-Policy-backups" -ItemType "Directory"
-
 Write-S3Object -BucketName $Bucket_Name -KeyPrefix "\" -Folder "$Desktop\Group-Policy-backups"
 
-Backup-GPO -All -Path C:\Users\Administrator\desktop\Group-Policy-backups
+$Invalid_Chars = ':\\/' + [RegEx]::Escape(-join [IO.Path]::InvalidPathChars)
 
-Write-S3Object -BucketName $Bucket_Name -KeyPrefix "\Group-Policy-backups" -Folder "C:\Users\Administrator\desktop\Group-Policy-backups" -Recurse
+New-Item -Path $Desktop -Name "Group-Policy-backups" -ItemType "Directory"
+
+$Backup_Directory = '$Desktop\Group-Policy-backups'
+
+Get-GPO -All | ForEach-Object {
+  $Name = $_.DisplayName -replace "[$invalidChars]", '_'
+  Write-Host "Backing up GPO named $Name to $Bucket_Name" -ForegroundColor Green
+  $GPO_Directory = Join-Path $Backup_Directory -ChildPath $Name
+  New-Item $GPO_Directory -Type Directory | Out-Null
+  Backup-GPO -Guid $_.Id -Path $GPO_Directory
+}
+
+Write-S3Object -BucketName $Bucket_Name -KeyPrefix "\Group-Policy-backups" -Folder "$Desktop\Group-Policy-backups" -Recurse
+
+Get-ADUser -Filter * | Select Name, GivenName, SamAccountName, DistinguishedName | Export-CSV "$Desktop\Users.csv"
+
+Write-Host "Backing up Active Directory users to bucket $Bucket_Name as a csv file." -ForegroundColor Green
+
+Write-S3Object -BucketName $Bucket_Name -KeyPrefix "\" -Key "Users.csv" -File "$Desktop\Users.csv"
