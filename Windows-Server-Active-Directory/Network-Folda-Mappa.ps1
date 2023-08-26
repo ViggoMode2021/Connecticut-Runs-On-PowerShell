539f35a8feef819b78d8fd0edef2dfba87383408
Ryan Viglione <86791607+ViggoMode2021@users.noreply.github.com>
2023-08-26 14:00:55 -0400

    Update Network-Folda-Mappa.ps1

--- Windows-Server-Active-Directory/Network-Folda-Mappa.ps1
+++ Windows-Server-Active-Directory/Network-Folda-Mappa.ps1
@@ -1,18 +1,44 @@
 # Folder mapper # Reference https://www.youtube.com/watch?v=OVln4mAqP30
 
+# Remove-PSDrive -Name L
+
+# Get-ADGroup -Filter "*" | Remove-ADGroup
+
+# Get-ADGroup -Filter 'Name -like "Faculty-Share"' | Remove-ADGroup
+
+# Steps:
+
+#------------------------------------#
+
+# 1.) Create security groups 
+
+# 2.) Create folder
+
+# 3.) Create SMB share 
+
+# 4.) Map with letters
+
+#------------------------------------#
+
 $Domain = Get-ADDomain | Select -expand Name
 
-Get-ADUser -Properties Description -Filter {Description -like "Math"} -SearchBase
+New-ADGroup -Name MathFileShare -GroupCategory Security -GroupScope DomainLocal -Path "OU=FacultyAccounts,DC=$Domain,DC=org" | Get-ADUser -Properties Description -Filter {Description -like "History"} -SearchBase "OU=FacultyAccounts,DC=$Domain,DC=org" | ForEach-Object { Add-ADGroupMember -Identity $_.SamAccountName -Members $_ }
 
-New-ADGroup -Name Math-FileShare -GroupCategory Security -GroupScope DomainLocal -Path "OU=FacultyAccounts,DC=$Domain,DC=org"
+New-ADGroup -Name HistoryFileShare -GroupCategory Security -GroupScope DomainLocal -Path "OU=FacultyAccounts,DC=$Domain,DC=org"
 
-Get-ADUser -Properties Description -Filter {Description -like "History"} -SearchBase
+Get-ADUser -Properties Description -Filter {Description -like "Math"} -SearchBase 
 
-New-ADGroup -Name History-FileShare -GroupCategory Security -GroupScope DomainLocal -Path "OU=FacultyAccounts,DC=$Domain,DC=org"
 
-New-ADGroup -Name Faculty-FileShare -GroupCategory Security -GroupScope DomainLocal -Path "OU=FacultyAccounts,DC=$Domain,DC=org"
+New-ADGroup -Name HistoryFileShare -GroupCategory Security -GroupScope DomainLocal -Path "OU=FacultyAccounts,DC=$Domain,DC=org"
 
-New-Item -Type Directory -Name Faculty-FileShares -Path "C:\"
+Get-ADUser -Filter {Description -like "History"} | % { Add-ADGroupMember 'HistoryFileShare' -Members $_ }
+
+
+
+
+New-Item -Type Directory -Name Math-FileShare -Path "C:\"
+
+New-Item -Type Directory -Name History-FileShare -Path "C:\"
 
 New-SMBShare `
     –Name Faculty-FileShares `
