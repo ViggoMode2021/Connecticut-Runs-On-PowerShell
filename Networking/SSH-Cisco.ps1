69b50fcfb22d5bb3c6bf8314834506c120376be3
Ryan Viglione <86791607+ViggoMode2021@users.noreply.github.com>
2023-12-05 21:42:30 -0500

    Update SSH-Cisco.ps1

--- Networking/SSH-Cisco.ps1
+++ Networking/SSH-Cisco.ps1
@@ -2,14 +2,16 @@
 
 # Example - "192.168.0.11"
 
-$IP_Of_Device = Read-Host "What is the IP address of the device you want to SSH to?"
-
 Import-Module -Name Posh-SSH
+
+$IP_Of_Device = Read-Host "What is the IP address of the device you want to SSH to?"
 
 $SSH_Session = New-SSHSession -ComputerName $IP_Of_Device -AcceptKey -Credential (Get-Credential admin)
 
 $SSH_Stream = $SSH_Session.Session.CreateShellStream("dumb", 0, 0, 0, 0, 1000)
-$SSH_Stream.Write("Terminal Length 0 `n")
 $SSH_Stream.Write("Show Running-Config `n")
+$SSH_Stream.Write(" ")
+$SSH_Stream.Write(" ")
+$SSH_Stream.Write(" ")
 Sleep 5
-$SSH_Stream.Read()
+$SSH_Stream.Read() | Out-File .\cisco.txt
