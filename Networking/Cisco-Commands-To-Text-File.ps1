7698d23c8e305875af559e5b51f31f8d5eec9798
Ryan Viglione <86791607+ViggoMode2021@users.noreply.github.com>
2023-12-12 07:29:33 -0500

    Update Cisco-Commands-To-Text-File.ps1

--- Networking/Cisco-Commands-To-Text-File.ps1
+++ Networking/Cisco-Commands-To-Text-File.ps1
@@ -21,16 +21,20 @@ $Cisco_Commands.AddRange([String[]]("1.) hostname",
         "`n14.) show inmterfaces switchport",
         "`n15.) show interfaces trunk",
         "`n16.) show vlan",
-        "`n17.) show vlan brief"
+        "`n17.) show vlan brief",
+        "`n18.) show ip route",
+        "`n19.) show ip rip database"
     ))
 
-$Selected_Command = Read-Host "Please type number of command that you would like to send the results of to a text file."
+Write-Host $Cisco_Commands -ForegroundColor Green
 
-Write-Host $Cisco_Commands
+$Selected_Command = Read-Host "### Please type number of command that you would like to send the results of to a text file. ###"
 
 $Selected_Command = [int]$Selected_Command
 $Selected_Command = $Selected_Command - 1
-$Cisco_Commands[$Selected_Command] 
+$Selected_Command = $Cisco_Commands[$Selected_Command]
+$Remove_Closing_Parenthesis = $Selected_Command.IndexOf(") ")      
+$Selected_Command = $Selected_Command.Substring($Remove_Closing_Parenthesis + 1)
 
 if (Get-Module -ListAvailable -Name Posh-SSH) {
     Write-Host "Posh-SSH is installed." -ForegroundColor Green
@@ -46,7 +50,7 @@ $IP_Of_Device = Read-Host "What is the IP address of the device you want to SSH 
 
 $SSH_Session = New-SSHSession -ComputerName $IP_Of_Device -AcceptKey -Credential (Get-Credential admin)
 
-$SSH_Stream = $SSH_Session.Session.CreateShellStream("dumb", 0, 0, 0, 0, 1000)
-$SSH_Stream.Write("Show Running-Config `n")
+$SSH_Stream = $SSH_Session.Session.CreateShellStream("", 0, 0, 0, 0, 1000)
+$SSH_Stream.Write("$Selected_Command `n")
 Start-Sleep 5
-$SSH_Stream.Read() | Out-File .\cisco.txt
+$SSH_Stream.Read() | Out-File .\$Selected_Command.txt
